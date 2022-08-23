041859354bb78301a7f32a0a387a97ff1e93bf0b
Leonardo Oliveira <leonardoliveira@outlook.com>
2022-08-22 21:07:49 -0300

    Inserido comando para conectar com MYSQL, usando o mindsdb.

---
+++ MySQL Com Machine Learning/Conexao-com-mindsdb.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE display_name  --- nome do banco de dados. 
+WITH ENGINE = "mysql",        --- nome do conector
+PARAMETERS = {
+    "user": " ",              --- seu usuário.
+    "password": " ",          --- sua senha.
+    "host": " ",              --- endereço do servidor. 
+    "port": "3306",           --- número da porta.
+    "database": " "           --- nome da base de dados.
+};
---
+++ MySQL Com Machine Learning/README.md
@@ -0,0 +1,6 @@
+# Mysql-and-BigQuery-Scripts
+
+## Machine Learning com SQL
+
+Repositório para scripts SQL, com a ferramenta [MindsDB](https://mindsdb.com/), que é um motor SQL, voltado para rodar algoritmos de aprendizado de máquinas.
+
